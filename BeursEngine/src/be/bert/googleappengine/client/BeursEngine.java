582f50d5ef055f42ede4397fb34eb1f3723211ac
Bert Godon <bert.godon@gmail.com>
2014-09-23 21:21:45 +0200

    Test

--- BeursEngine/src/be/bert/googleappengine/client/BeursEngine.java
+++ BeursEngine/src/be/bert/googleappengine/client/BeursEngine.java
@@ -39,6 +39,7 @@ public class BeursEngine implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		
 		final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
 		nameField.setText("GWT User");
